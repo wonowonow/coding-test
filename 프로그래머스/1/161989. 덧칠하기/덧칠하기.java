a86ff0928d8e2eddbd0e3499706f22ac0aa78f57
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-11-30 09:16:25 +0900

    [level 1] Title: 덧칠하기, Time: 55.13 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 0; j < section.length; j++) {
+                
+                if(section[j] == i) {
+                    
+                    answer++;
+                    i += m;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
