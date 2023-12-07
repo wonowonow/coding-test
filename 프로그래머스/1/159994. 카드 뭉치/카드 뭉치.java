81c2670bcbea507d2399e62a4ea03bd94004f4a6
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-07 14:38:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,3 +1,25 @@
+// class Solution {
+//     public String solution(String[] cards1, String[] cards2, String[] goal) {
+//         String answer = "";
+        
+//         int cards1Count = 0;
+//         int cards2Count = 0;
+        
+//         for(String word : goal) {
+            
+//             if(cards1Count < cards1.length && cards1[cards1Count].equals(word)) {
+//                 cards1Count++;
+//             } else if(cards2Count < cards2.length && cards2[cards2Count].equals(word)) {
+//                 cards2Count++;
+//             } else {
+//                 return "No";
+//             }
+//         }
+        
+//         return "Yes";
+//     }
+// }
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
@@ -5,11 +27,11 @@ class Solution {
         int cards1Count = 0;
         int cards2Count = 0;
         
-        for(String word : goal) {
+        for(int i = 0; i < goal.length; i++) {
             
-            if(cards1Count < cards1.length && cards1[cards1Count].equals(word)) {
+            if(cards1Count < cards1.length && cards1[cards1Count].equals(goal[i])) {
                 cards1Count++;
-            } else if(cards2Count < cards2.length && cards2[cards2Count].equals(word)) {
+            } else if(cards2Count < cards2.length && cards2[cards2Count].equals(goal[i])) {
                 cards2Count++;
             } else {
                 return "No";
