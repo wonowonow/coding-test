24dce8378ad34914a771b58558c6deb9514e9250
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-07 14:27:22 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        
+        int cards1Count = 0;
+        int cards2Count = 0;
+        
+        for(String word : goal) {
+            
+            if(cards1Count < cards1.length && cards1[cards1Count].equals(word)) {
+                cards1Count++;
+            } else if(cards2Count < cards2.length && cards2[cards2Count].equals(word)) {
+                cards2Count++;
+            } else {
+                return "No";
+            }
+        }
+        
+        return "Yes";
+    }
+}
