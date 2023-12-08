4aa08499cdafe5e475e037b22bfbf0862efde045
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-08 09:46:03 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        if(n < a) {
-            return answer;
-        }
         for(int i = n; i >= a; i -= a) {
             i += b;
             answer += b;
