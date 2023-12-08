9cbb15a96d1ca3c887e824b9532dae48bf9a6b0c
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-08 09:47:49 +0900

    [level 1] Title: 체육복, Time: 0.38 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -4,10 +4,8 @@ class Solution {
         int answer = n - lost.length;
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        // 도난 당한 학생 기준으로 바로 아래 번호에 있는 애한테 먼저 빌려야함
         for(int i = 0; i < lost.length; i++) {
             for(int j = 0; j < reserve.length; j++) {
-                // 여벌 체육복을 가져온 학생이 도난을 당했다면?
                 if(lost[i] == reserve[j]) {
                     answer++;
                     lost[i] = -1;
@@ -18,7 +16,6 @@ class Solution {
         }
         for(int i = 0; i < lost.length; i++) {
             for(int j = 0; j < reserve.length; j++) {
-                // 만약 lost가 있는데 앞 번호에 여분의 체육복을 갖고 있는 학생이 있다면?
                 if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
                     if (reserve[j] != -1) {
                         answer++;
