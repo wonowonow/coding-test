11c24f539eccb26d2b0ea71398d2d13e949e869a
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-08 09:40:48 +0900

    [level 1] Title: 과일 장수, Time: 0.40 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        List<Integer> scoreList = new ArrayList<>();
+        for(int i : score) {
+            scoreList.add(i);
+        }
+        scoreList.sort(Comparator.reverseOrder());
+        
+        for(int i = 0; i < scoreList.size();) {
+            int[] appleBox = new int[m];
+            for(int j = 0; j < appleBox.length; j++) {
+                if(i < scoreList.size()) {
+                    appleBox[j] = scoreList.get(i);
+                    i++;
+                }
+            }
+            answer += appleBox[appleBox.length-1] * m;
+        }
+        
+        return answer;
+    }
+}
