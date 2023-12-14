0905ae9ece17895e34af95f41c901481bb2389cd
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-14 15:41:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 11.92 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String s2 = s;
+        String s3 = s2;
+        
+        for(int i = 0; i < s.length()-1; i++) {
+            s3 = s2;
+            
+            for(int j = 0; j < s.length(); j++) {
+                s3 = s3.replace("()", "");
+                s3 = s3.replace("{}", "");
+                s3 = s3.replace("[]", "");
+            }
+            
+            if(s3.length() == 0) {
+                answer++;
+            }
+            
+            s2 = s2.substring(1) + s2.charAt(0);
+        }
+        return answer;
+    }
+}
