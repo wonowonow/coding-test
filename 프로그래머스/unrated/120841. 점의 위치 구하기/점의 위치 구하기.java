e17b1e04c6060a7c390ef7ad443161aed03fc272
WONOW <123870616+wonowonow@users.noreply.github.com>
2023-12-26 09:25:30 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 1;
+        
+        if(dot[0] < 0 && dot[1] > 0) {
+            answer = 2;
+        }
+        
+        if(dot[0] < 0 && dot[1] < 0) {
+            answer = 3;
+        }
+        
+        if(dot[0] > 0 && dot[1] < 0) {
+            answer = 4;
+        }
+        
+        return answer;
+    }
+}
