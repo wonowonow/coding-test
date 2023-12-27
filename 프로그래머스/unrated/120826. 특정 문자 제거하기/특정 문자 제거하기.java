80418ec08566ac2713c9be576db0583d0b4f3b2b
WONOW <123870616+wonowonow@users.noreply.github.com>
2023-12-27 09:06:28 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        my_string = my_string.replaceAll(letter, "");
+        String answer = my_string;
+        return answer;
+    }
+}
