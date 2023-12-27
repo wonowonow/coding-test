49b3605fb0c7e77e55c375141ef6e521e74e8530
WONOW <123870616+wonowonow@users.noreply.github.com>
2023-12-27 09:45:39 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i * i <= n; i++) {
+            if(n % i == 0) {
+                answer++;
+                if (i != n / i) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
