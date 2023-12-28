1ec76191411dec54f2c63d743d1eae07d0c7f8c1
WONOW <123870616+wonowonow@users.noreply.github.com>
2023-12-28 09:42:51 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -1,24 +1,12 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0; 
-        
-        for(int i = 0; i < 10000; i++) {
-            
-            if(hp - 5 >= 0) {
-                answer++;
-                hp -= 5;
-            } else if(hp - 3 >= 0) {
-                answer++;
-                hp -= 3;
-            } else if(hp - 1 >= 0) {
-                answer++;
-                hp -= 1;
-            }
-            
-            if(hp == 0) {
-                break;
-            }
-        }
+        int answer = hp / 5;
+        hp %= 5;
+
+        answer += hp / 3;
+        hp %= 3;
+
+        answer += hp / 1;
 
         return answer;
     }
