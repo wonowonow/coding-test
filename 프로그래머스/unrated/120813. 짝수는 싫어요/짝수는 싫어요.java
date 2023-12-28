0c34617f22b7bf094156b235954a97aac35b6ec1
WONOW <123870616+wonowonow@users.noreply.github.com>
2023-12-28 09:14:54 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n - 1) / 2 + 1];
+        
+        for(int i = 0; i < answer.length; i++) {
+            
+            answer[i] = i * 2 + 1;
+        }
+        
+        return answer;
+    }
+}
