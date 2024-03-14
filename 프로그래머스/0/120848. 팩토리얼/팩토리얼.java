98149f2a57a07b1184b1c6e92d6cced02aeaeb07
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-03-15 00:44:50 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        
+        int i = 1;
+        int factorial = 1;
+        
+        while (factorial <= n) {
+            i++;
+            factorial *= i;
+        }
+        
+        return i - 1;
+    }
+}
