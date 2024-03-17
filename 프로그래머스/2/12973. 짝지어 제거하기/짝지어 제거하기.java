99634d0779b6231555f9f08e5ee5fdb9ba4f14d0
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-03-18 02:25:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.45 ms, Memory: 59 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+            } else if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        
+        if (stack.isEmpty()) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
