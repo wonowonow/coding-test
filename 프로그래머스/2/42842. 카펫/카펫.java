248ec1da7da0752f47fd8796cf699de24509f58d
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-03-18 02:53:59 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {0, 0};
+
+        int sum = brown + yellow;
+        int x = sum;
+        int y = 3;
+        
+        while (true) {
+            if (x * y > sum) {
+                x--;
+            } else if (x * y < sum) {
+                y++;
+            } else {
+                int[] size = {x, y};
+                if (canMiddle(brown, yellow, size)) {
+                    answer[0] = x;
+                    answer[1] = y;
+                    break;
+                } else {
+                    y++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public boolean canMiddle(int brown, int yellow, int[] size) {
+        // 24, 24 / 3, 16
+        int needBrownMin = (size[0] * 2) + (size[1] * 2) - 4;
+        int needYellowMin = size[0] * size[1] - needBrownMin;
+        
+        if (brown == needBrownMin && yellow == needYellowMin) {
+            return true;
+        }
+        
+        return false;
+    }
+}
