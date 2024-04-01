6b0b141df0198f5f1e7527319df5ac914bcb3af6
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-04-01 09:27:51 +0900

    [level 0] Title: 순서 바꾸기, Time: 3.04 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = n; i < num_list.length; i++) {
+            answer.add(num_list[i]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            answer.add(num_list[i]);
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
