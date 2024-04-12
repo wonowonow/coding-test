bb962f359e9aaf68815daedfe8fc410d91e1260c
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-04-12 22:13:14 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            
+            for (int j = 0; j < numbers.length; j++) {
+                
+                if (i == j) {
+                    continue;
+                }
+                
+                answer = Math.max(numbers[i] * numbers[j], answer);
+            }
+        }
+        
+        return answer;
+    }
+}
