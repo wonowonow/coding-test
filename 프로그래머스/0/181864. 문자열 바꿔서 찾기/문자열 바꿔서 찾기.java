682c3d31f232c28b0846f6a0e00012b833bd02f6
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-04-13 22:48:32 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        int answer = 0;
-        
+
         return myString.contains(pat.replace("A", "C").replace("B", "A").replace("C", "B")) ? 1 : 0;
     }
 }
