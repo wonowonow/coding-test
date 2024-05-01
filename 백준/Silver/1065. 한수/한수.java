26d023cc7db34c8e842fe17e7e2dd943ec2edbde
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-05-01 10:31:37 +0900

    [Silver IV] Title: 한수, Time: 256 ms, Memory: 19368 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -12,26 +12,24 @@ public class Main {
 
             String str = String.valueOf(i);
 
-            if (str.length() == 1 || str.length() == 2) {
+            if (str.length() <= 2) {
                 answer++;
-            } else if (str.length() == 3) {
+            } else {
                 String[] arr = str.split("");
                 int cha = Integer.parseInt(arr[0]) - Integer.parseInt(arr[1]);
-                if (Integer.parseInt(arr[1]) - Integer.parseInt(arr[2]) == cha) {
-                    answer++;
-                }
-            } else if (str.length() == 4) {
-                String[] arr = str.split("");
-                int cha = Integer.parseInt(arr[0]) - Integer.parseInt(arr[1]);
-                if (Integer.parseInt(arr[1]) - Integer.parseInt(arr[2]) == cha) {
-                    if (Integer.parseInt(arr[2]) - Integer.parseInt(arr[3]) == cha) {
-                        answer++;
+                boolean check = true;
+                for (int j = 1; j < arr.length; j++) {
+                    if (Integer.parseInt(arr[j - 1]) - Integer.parseInt(arr[j]) != cha) {
+                        check = false;
+                        break;
                     }
+                }
+                if (check) {
+                    answer++;
                 }
             }
         }
 
         System.out.print(answer);
     }
-
 }
