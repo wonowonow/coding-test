6f3e72a1b5dcb4f0090093662e13180437df5436
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-05-24 07:49:22 +0900

    [Bronze V] Title: 시험 성적, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        
+        if (score >= 90 && score <= 100) {
+            System.out.println("A");
+        } else if (score >= 80 && score < 90) {
+            System.out.println("B");
+        } else if (score >= 70 && score < 80) {
+            System.out.println("C");
+        } else if (score >= 60 && score < 70) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+        
+        br.close();
+    }
+}
