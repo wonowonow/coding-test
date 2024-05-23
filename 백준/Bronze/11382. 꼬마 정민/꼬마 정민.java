9db3bab2c76f610bf4b82760521a8ce91207606f
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-05-24 07:47:58 +0900

    [Bronze V] Title: 꼬마 정민, Time: 144 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        long sum = A + B + C;
+        
+        bw.write(sum + "\n");
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
