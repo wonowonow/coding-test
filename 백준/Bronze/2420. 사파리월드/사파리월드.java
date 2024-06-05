f2be6170f9070ae613e1ece99d8082132b908590
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-06-05 14:57:52 +0900

    [Bronze V] Title: 사파리월드, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        
+        System.out.print(Math.abs(a - b));
+    }
+}
