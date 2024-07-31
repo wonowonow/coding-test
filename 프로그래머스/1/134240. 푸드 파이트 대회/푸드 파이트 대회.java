4a1881a4a59a3521ad6688fff4c5d2b42aacac4b
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-07-31 10:31:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 26.37 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -4,16 +4,20 @@ class Solution {
         String right = "";
         
         for(int i = 1; i < food.length; i++) {
-            for(int j = 1; j <= food[i] / 2; j++) {
+            for(int j = 0; j < food[i] / 2; j++) {
                 left += i;
             }
         }
+        
+        System.out.println(left);
 
         for(int i = food.length-1; i > 0; i--) {
-            for(int j = 1; j <= food[i] / 2; j++) {
+            for(int j = 0; j < food[i] / 2; j++) {
                 right += i;
             }
         }
+        
+        System.out.print(right);
         
         String answer = left + "0" + right;
         
