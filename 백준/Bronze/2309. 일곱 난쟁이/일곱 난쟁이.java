0f6cce7976faf644a75efab9f0db9067b7fccae5
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-11-11 11:59:09 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int[] arr = new int[9];
+        int spy1 = 10;
+        int spy2 = 11;
+        boolean check = false;
+        
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            
+            if (!check) {
+                for (int j = i + 1; j < arr.length; j++) {
+
+                    if (sum - (arr[i] + arr[j]) == 100) {
+                        spy1 = i;
+                        spy2 = j;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < 9; i++) {
+            
+            if (i != spy1 && i != spy2) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
