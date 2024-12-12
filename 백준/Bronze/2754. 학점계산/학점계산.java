077851e19cc85239bee6beeb243e6cca198135aa
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-12-12 15:33:21 +0900

    [Bronze III] Title: 학점계산, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -6,31 +6,27 @@ public class Main {
         
         Scanner sc = new Scanner(System.in);
         String score = sc.nextLine();
-        if (score.equals("F")) {
-            System.out.print("0.0");
-        } else {
-            float answer = 0.0f;
-            for (char c : score.toCharArray()) {
-                
-                if (c == 'A') {
-                    answer = 4.0f;
-                } else if (c == 'B') {
-                    answer = 3.0f;
-                } else if (c == 'C') {
-                    answer = 2.0f;
-                } else if (c == 'D') {
-                    answer = 1.0f;
-                }
-                
-                if (c == '+') {
-                    answer += 0.3f;
-                } else if (c == '-') {
-                    answer -= 0.3f;
-                }
+        
+        float answer = 0.0f;
+        for (char c : score.toCharArray()) {
+
+            if (c == 'A') {
+                answer = 4.0f;
+            } else if (c == 'B') {
+                answer = 3.0f;
+            } else if (c == 'C') {
+                answer = 2.0f;
+            } else if (c == 'D') {
+                answer = 1.0f;
             }
-            
-            System.out.print(answer);
-            
+
+            if (c == '+') {
+                answer += 0.3f;
+            } else if (c == '-') {
+                answer -= 0.3f;
+            }
         }
+            
+        System.out.print(answer);
     }
 }
