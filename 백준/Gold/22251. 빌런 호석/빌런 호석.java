6e9fee181b30a2cf9cde52d7da6cdc40408de111
WONOW <123870616+wonowonow@users.noreply.github.com>
2024-12-30 18:51:21 +0900

    [Gold V] Title: 빌런 호석, Time: 480 ms, Memory: 70200 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.java
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.java
@@ -23,28 +23,37 @@ public class Main {
         
         String cur = String.valueOf(X);
         
+        // 현재 디스플레이에 표현되는 수 만큼 0 채우기
         while (cur.length() < K) {
             cur = "0" + cur;
         }
         
         for (int i = 1; i <= N; i++) {
             
+            // 같은 층이면 건너 뛰기
             if (i == X) {
                 continue;
             }
             
+            // 호석이가 테스트 해 볼 층 수
             String hoseock = String.valueOf(i);
             
+            // 빌런 디스플레이에 표현되는 수 만큼 0 채우기
             while(hoseock.length() < K) {
                 hoseock = "0" + hoseock;
             }
             
+            // 반전 수 체크하기 위한 변수
             int cnt = 0;
             
+            // K 자리수 까지 돌기
             for (int j = 0; j < K; j++) {
+                // 현재 j번째 Led
                 String curLed = num[cur.charAt(j) - '0'];
+                // 호석 j번째 Led
                 String hoLed = num[hoseock.charAt(j) - '0'];
                 
+                // 현재랑 호석이랑 Led 바뀐 곳 체크
                 for (int k = 0; k < 7; k++) {
                     if (curLed.charAt(k) != hoLed.charAt(k)) {
                         cnt++;
@@ -52,6 +61,7 @@ public class Main {
                 }
             }
             
+            // P보다 바뀐게 적다면 answer++;
             if (cnt <= P) {
                 answer++;
             }
